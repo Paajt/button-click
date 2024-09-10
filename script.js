2138563c71808b76ebb95b41c40c9b206350765f
Patrik <pat_p3@hotmail.com>
2024-09-10 20:26:54 +0200

    Added comment on reset function and cleared up whitespace

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // function to change h1 message
 function changeMessage() {
-
    document.getElementById("message").innerHTML = "Goodbye World";
-
 }
 
+// function to reset h1 message
 function resetButton() {
-
     document.getElementById("message").innerHTML = "Hello World"
-    
 }
